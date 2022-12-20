e9066377a784aad4fa67323398f77bf4de314ce1
adiljin <adiljins@gmail.com>
2022-12-20 15:02:47 -0500

    Changed: -UserController -UserNotFountAdvice -UserNotFoundException

--- src/main/java/com/adiljins/fullstackbackend/controller/UserController.java
+++ src/main/java/com/adiljins/fullstackbackend/controller/UserController.java
@@ -1,16 +1,14 @@
 package com.adiljins.fullstackbackend.controller;
-
+import com.adiljins.fullstackbackend.exception.UserNotFoundException;
 import com.adiljins.fullstackbackend.model.User;
 import com.adiljins.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class UserController {
 
     @Autowired
@@ -26,4 +24,27 @@ public class UserController {
         return userRepository.findAll();
     }
 
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(newUser.getUsername());
+            user.setName(newUser.getName());
+            user.setEmail(newUser.getEmail());
+            return userRepository.save(user);
+        }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if (!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id " + id + " has been deleted";
+    }
 }
